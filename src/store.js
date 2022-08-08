75518aae1385de557575fa8be5cff15b47b66d15
JOSEPH BESKY J <104523855+JOSEPHBESKY@users.noreply.github.com>
2022-08-08 06:57:55 -0700

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  cartslice  from "./slice";
+import favslice from "./favslice"
 export default configureStore({
     reducer:{
         cart:cartslice,
+        fav:favslice
     }
 })
