2684d98ca71f9d15df8831ff4edff3dc9e4a8a45
JOSEPH BESKY J <104523855+JOSEPHBESKY@users.noreply.github.com>
2022-08-08 06:56:14 -0700

    Update favSlice.js

--- src/favSlice.js
+++ src/favSlice.js
@@ -1,3 +1,18 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-export
+export const favslice=createSlice({
+    name:'fav',
+    initialState:{
+        favidems:[]
+    },
+    reducer:{
+        addtocart:(state,action)=>{
+            console.log(state,action);
+        state.favidems=action.payload
+        }
+
+    }
+})
+
+export const {addtofav}=favslice.actions
+export default favslice.reducer
