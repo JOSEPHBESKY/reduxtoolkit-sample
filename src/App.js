fb459a6cffbbea2bcda17eff6f793eb3110dcb3b
JOSEPH BESKY J <104523855+JOSEPHBESKY@users.noreply.github.com>
2022-08-08 07:00:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import {useSelector,useDispatch} from 'react-redux'
 import {addtocart} from'./slice'
+import {addtofav} from './favslice
 function App() {
   const state= useSelector((state)=>state)
   const dispatch=useDispatch()
   const addproduct=()=>{
     dispatch(addtocart([{id:1,name:'aaa'}]))
   }
+  const addtofavrout=()=>{
+    dispatch(addtofav({name:"jose"}))}
   console.log(state);
   return (
     <div>react
     <button onClick={()=>addproduct()}>addcart</button>
+    <button onClick={()=>addtofavrout()}>addfav</button>
 
     </div>
   )
